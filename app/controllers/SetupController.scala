83368311ddd38801f3eb3fe335126c1becf8fc90
sidharth <sidharth.khattri@gmail.com>
2017-08-18 15:14:28 +0530

    added route to send credentials

--- app/controllers/SetupController.scala
+++ app/controllers/SetupController.scala
@@ -1,7 +1,9 @@
 package controllers
 
-import javax.inject.{Inject, Singleton}
+import javax.inject.{Named, Inject, Singleton}
 
+import actors.EmailActor
+import akka.actor.ActorRef
 import models.UsersRepository
 import play.api.mvc.{Action, AnyContent}
 import utilities.PasswordUtility
@@ -11,12 +13,30 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 @Singleton
 class SetupController @Inject()(usersRepository: UsersRepository,
-                                controllerComponents: KnolxControllerComponents
+                                controllerComponents: KnolxControllerComponents,
+                                @Named("EmailManager") emailManager: ActorRef
                                ) extends KnolxAbstractController(controllerComponents) {
 
   val usersSource = scala.io.Source.fromFile("/home/ubuntu/users.csv")
   val users = usersSource.getLines.toList
   usersSource.close()
+
+  def sendCredentials: Action[AnyContent] = adminAction { implicit request =>
+    users foreach { user =>
+      val usernamePassword = user.split(",")
+      val (username, password) = (usernamePassword.head, usernamePassword(1))
+
+      val body =
+        s"""Knolx Portal: http://knolx.knoldus.com
+            |Username: $username
+            |Password: $password
+        """.stripMargin
+
+      emailManager ! EmailActor.SendEmail(List(username), "support@knoldus.com", "Knolx Portal Credentials", body)
+    }
+
+    Ok("sent!")
+  }
 
   def addUsers(): Action[AnyContent] = adminAction.async { implicit request =>
     val insertResult = users map { user =>
